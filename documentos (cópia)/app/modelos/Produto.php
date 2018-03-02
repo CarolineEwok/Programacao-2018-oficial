30fa635a7e44fa26682e81e8b0d4b792ae08069f
Letícia Caroline <caroline.ewok@gmail.com>
2018-03-02 09:29:32 -0300

    Add files via upload

---
+++ documentos (cópia)/app/modelos/Produto.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aluno
+ * Date: 02/03/18
+ * Time: 08:55
+ */
+
+class Produto
+{
+    private $id;
+    private $nome;
+    private $descricao;
+    private $foto;
+    private $preco;
+    private $idcategoria;
+
+    
+ 
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+
+
+    public function getFoto()
+    {
+        return $this->foto;
+    }
+
+    public function setFoto($foto)
+    {
+        $this->foto = $foto;
+    }
+
+
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    public function setPreco($preco)
+    {
+        $this->preco = $preco;
+    }
+
+    public function getIdcategoria()
+    {
+        return $this->idcategoria;
+    }
+
+
+    public function setIdcategoria($idcategoria)
+    {
+        $this->idcategoria = $idcategoria;
+    }
+
+}
---
+++ documentos (cópia)/testes/teste_categoria.php
@@ -0,0 +1,9 @@
+<?php
+
+require '../app/modelos/Categoria.php';
+
+    $cat1 = new Categoria(1, 'eletronicos', 'smartphone');
+   
+    var_dump($cat1);
+    
+    
